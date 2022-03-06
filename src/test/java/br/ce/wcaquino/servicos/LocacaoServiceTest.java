161eeedad0b047a0b5ef0797fb9fb2ab094da05e
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-03-06 14:36:00 +0000

    CoreMatchers.equalTo utilizado

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -3,16 +3,15 @@ package br.ce.wcaquino.servicos;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
 import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
 import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LocacaoServiceTest {
@@ -27,7 +26,7 @@ class LocacaoServiceTest {
         Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
 //		Then
-        assertThat(locacao.getValor(), is(5.)); // Verifica o preço da locação
+        assertThat(locacao.getValor(), is(equalTo(5.))); // Verifica o preço da locação
         assertTrue(isMesmaData(locacao.getDataLocacao(), new Date())); // Verifica a data da locação
         assertTrue(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1))); // Verifica a data de retorno da locação
     }

0c8cb051ca5fc166677ebb402905c44c91bd4cf8
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-03-08 13:32:27 +0000

    Retirado o tratamento da exceção (try/catch) e mudado apenas para lançamento de exceção (throws)

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -4,7 +4,6 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import org.assertj.core.api.SoftAssertions;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -15,27 +14,30 @@ import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
 class LocacaoServiceTest {
     SoftAssertions softAssertions = new SoftAssertions();
 
+    /**
+     * Caso o teste não esteja esperando uma exceção, a melhor opção é deixar que o JUnit a trate.
+     *
+     * @throws Exception exceção que fará o teste dar erro.
+     */
     @Test
-    void main() {
+    void main() throws Exception {
 //        Given
         Usuario usuario = new Usuario("Wanderley");
         Filme filme = new Filme("Mother", 1, 5.);
         Date date = new Date();
         LocacaoService locacaoService = new LocacaoService();
 
-        try {
+
 //		When
-            Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+        Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+
 //		Then
         softAssertions.assertThat(locacao.getValor()).as("Preço da locação incorreto").isEqualTo(5);
         softAssertions.assertThat(locacao.getDataLocacao()).as("Data da locação incorreta").isEqualTo(locacaoService.dataLocacao);
         softAssertions.assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1))).isEqualTo(true);
 
         softAssertions.assertAll();
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            Assertions.fail("Não deveria lançar exceção");
-        }
+
 
     }
 }

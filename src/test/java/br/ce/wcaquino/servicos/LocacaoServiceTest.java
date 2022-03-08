45c295b5433b6ee9dbe1f8a09bbfff90214c0f69
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-03-08 17:07:33 +0000

    - Inserida a abordagem do qual um teste está a espera de uma exceção sob as seguintes soluções:  * Elegante  * Robusta - Retirada dependência não utilizada do pom.xml

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -4,6 +4,10 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import org.assertj.core.api.SoftAssertions;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -12,7 +16,15 @@ import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
 import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
 
 class LocacaoServiceTest {
-    SoftAssertions softAssertions = new SoftAssertions();
+    SoftAssertions softAssertions;
+
+    /**
+     * Visto que há mais de um teste utilizando soft assertions, eu garanti que, para cada teste, uma nova instância é criada.
+     */
+    @BeforeEach
+    void setUp() {
+        softAssertions = new SoftAssertions();
+    }
 
     /**
      * Caso o teste não esteja esperando uma exceção, a melhor opção é deixar que o JUnit a trate.
@@ -37,7 +49,49 @@ class LocacaoServiceTest {
         softAssertions.assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1))).isEqualTo(true);
 
         softAssertions.assertAll();
+    }
 
+    /**
+     * Este está esperando uma exceção.
+     * Solução elegante.
+     *
+     * @throws Exception exceção que fará o teste dar erro.
+     */
+    @Test
+    public void filmeSemEstoqueElegante() throws Exception {
 
+//        Given
+        Usuario usuario = new Usuario("Wanderley");
+        Filme filme = new Filme("Mother", 0, 5.);
+        Date date = new Date();
+        LocacaoService locacaoService = new LocacaoService();
+
+//		Then
+        Assertions.assertThrows(Exception.class, () -> locacaoService.alugarFilme(usuario, filme));
+    }
+
+    /**
+     * Este está esperando uma exceção.
+     * Solução robusta.
+     * <p>
+     * Além de capturar a exceção, é possível verificar a mensagem que vem da mesma.
+     */
+    @Test
+    public void filmeSemEstoqueRobusta() {
+
+//        Given
+        Usuario usuario = new Usuario("Wanderley");
+        Filme filme = new Filme("Mother", 0, 5.);
+        Date date = new Date();
+        LocacaoService locacaoService = new LocacaoService();
+
+//		Then
+        try {
+            locacaoService.alugarFilme(usuario, filme);
+            Assertions.fail("Exceção deveria ter sido lançada"); // Força o teste a falhar
+        } catch (Exception exception) {
+            MatcherAssert.assertThat(exception.getMessage(), CoreMatchers.is("Não há este filme em estoque"));
+//            exception.printStackTrace();
+        }
     }
 }

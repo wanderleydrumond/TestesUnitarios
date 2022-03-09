d394f119afbfb1d7bb900934755b6e1bdf1bab48
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-03-09 12:07:04 +0000

    - Melhoria na abordagem de garantia de ordem de testes implementada

--- src/test/java/br/ce/wcaquino/OrdemTest.java
+++ src/test/java/br/ce/wcaquino/OrdemTest.java
@@ -1,26 +1,25 @@
 package br.ce.wcaquino;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 /**
  * Esta classe demonstra como garantir a ordem em que os testes serão executados caso haja necessidade de violar o princípio de independência dos mesmos.
  */
+@TestMethodOrder(MethodOrderer.MethodName.class) // Garante que os testes serão executados em ordem alfabética
 public class OrdemTest {
 
     public static int contador = 0;
 
+    @Test
     public void iniciar() {
         contador = 1;
     }
 
+    @Test
     public void verificar() {
         Assertions.assertEquals(1, contador);
-    }
-
-    @Test
-    public void garantirOrdemTestes() {
-        iniciar();
-        verificar();
     }
 }

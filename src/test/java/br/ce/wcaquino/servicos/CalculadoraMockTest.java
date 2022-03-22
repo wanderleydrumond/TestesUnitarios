c43974b741c3f70ff9390a440698c4c1ded7c070
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-03-22 14:29:03 +0000

    Abordagem da qual o Mockito manipula como o método deve se comportar.

---
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -0,0 +1,20 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class CalculadoraMockTest {
+    /**
+     * Exibe o resultado esperado pelo Mockito realizando a chamada do método da própria classe.
+     * <p>1 + 2 é 3 mas como eu defini que o resultado esperado é 5 é isso o que ele sempre esperará.</p>
+     */
+    @Test
+    @DisplayName("Exibir valores esperados pelo Mockito")
+    void testeValoresMockito() {
+        Calculadora calculadora = Mockito.mock(Calculadora.class);
+        Mockito.when(calculadora.somarDoisInteiros(1,2)).thenReturn(5);
+
+        System.out.println(calculadora.somarDoisInteiros(1,2));
+    }
+}

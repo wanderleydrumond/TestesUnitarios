908fddca1802c5a17068485714ab1e770b6512fc
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-04-19 09:43:38 +0100

    - Arquivos formatados - Estilos restantes alterados

--- src/javadoc/package-search-index.js
+++ src/javadoc/package-search-index.js
@@ -1,1 +1,12 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.ce.wcaquino"},{"l":"br.ce.wcaquino.builders"},{"l":"br.ce.wcaquino.daos"},{"l":"br.ce.wcaquino.entidades"},{"l":"br.ce.wcaquino.exceptions"},{"l":"br.ce.wcaquino.matchers"},{"l":"br.ce.wcaquino.servicos"},{"l":"br.ce.wcaquino.utils"}];updateSearchResults();
+packageSearchIndex = [
+  { l: "All Packages", u: "allpackages-index.html" },
+  { l: "br.ce.wcaquino" },
+  { l: "br.ce.wcaquino.builders" },
+  { l: "br.ce.wcaquino.daos" },
+  { l: "br.ce.wcaquino.entidades" },
+  { l: "br.ce.wcaquino.exceptions" },
+  { l: "br.ce.wcaquino.matchers" },
+  { l: "br.ce.wcaquino.servicos" },
+  { l: "br.ce.wcaquino.utils" },
+];
+updateSearchResults();
